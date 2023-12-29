fcb4c6d068435173449e62d193cb2bf9e9dffa81
LiAohua <liaohua@LiAohuadeMacBook-Pro.local>
2023-12-29 21:58:46 +0800

    update

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import '/src/assets/index.css'
+import { inject } from '@vercel/analytics';
 
+inject();
 createApp(App).use(router).mount('#app')
